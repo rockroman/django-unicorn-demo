9a73c88912e3015d1986880d58b121a649fb47b7
Roman Rakic <rock.rakic@gmail.com>
2023-07-11 19:50:50 +0100

    add compnent logic and create and delete functionality

--- component/components/comment.py
+++ component/components/comment.py
@@ -1,5 +1,24 @@
-from django_unicorn.components import UnicornView
+from component.models import Comment
+from django_unicorn.components import UnicornView, QuerySetType
 
 
 class CommentView(UnicornView):
-    pass
+    content: str = ""
+    comments: QuerySetType[Comment] = Comment.objects.none()
+
+    def mount(self):
+        self.comments = Comment.objects.all()
+
+    def add_comment(self):
+        Comment.objects.create(content=self.content)
+        self.comments = Comment.objects.all()
+        self.content = ""
+
+    def delete_comment(self, id):
+        comment = Comment.objects.get(id=id)
+        comment.delete()
+        self.comments = Comment.objects.all()
+
+    def delete_all(self):
+        Comment.objects.all().delete()
+        self.comments = Comment.objects.none()
--- component/urls.py
+++ component/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
 from . import views
+from .components.comment import CommentView
 
 app_name = "component"
 urlpatterns = [
     path("", include("django_unicorn.urls")),
     path("", views.index, name="index"),
+    path("", CommentView.as_view()),
 ]
