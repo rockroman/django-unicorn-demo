e18010b065755e4c7cfe630808f323e3aa951b33
Roman Rakic <rock.rakic@gmail.com>
2023-07-13 00:27:04 +0100

    add update functionality

--- component/components/comment.py
+++ component/components/comment.py
@@ -1,18 +1,34 @@
 from component.models import Comment
 from django_unicorn.components import UnicornView, QuerySetType
+from django.contrib import messages
 
 
 class CommentView(UnicornView):
     content: str = ""
     comments: QuerySetType[Comment] = Comment.objects.none()
+    comment_id: int = None
 
     def mount(self):
         self.comments = Comment.objects.all()
 
     def add_comment(self):
-        Comment.objects.create(content=self.content)
-        self.comments = Comment.objects.all()
+        if self.comment_id:
+            # Update existing comment
+            comment = Comment.objects.get(id=self.comment_id)
+            comment.content = self.content
+            if self.content:
+                comment.save()
+            else:
+                print("no comment")
+                messages.error(self.request, "comment body is none")
+                comment = Comment.objects.get(id=self.comment_id)
+
+        else:
+            # Create new comment
+            Comment.objects.create(content=self.content)
         self.content = ""
+        self.comment_id = None
+        self.comments = Comment.objects.all()
 
     def delete_comment(self, id):
         comment = Comment.objects.get(id=id)
@@ -22,3 +38,9 @@ class CommentView(UnicornView):
     def delete_all(self):
         Comment.objects.all().delete()
         self.comments = Comment.objects.none()
+
+    def edit_comment(self, id):
+        mycomment = Comment.objects.get(id=id)
+        self.content = str(mycomment)
+        self.content = mycomment.content
+        self.comment_id = id
